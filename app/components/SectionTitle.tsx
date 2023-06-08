4f3bbb37c92048527f0e7fcc5a0654483d77aadc
Sourav Choudhary <84680538+sourav0098@users.noreply.github.com>
2023-06-08 18:32:59 -0400

    changed ui

--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -10,7 +10,6 @@ export default function SectionTitle({ title }: Props) {
     <>
       <h2 className="font-titleFont text-2xl font-semibold flex items-center">
         {title}
-        <span className="hidden md:inline-flex md:w-60 lgl:w-72 h-[0.5px] bg-gray-700 ml-6"></span>
       </h2>
     </>
   );
